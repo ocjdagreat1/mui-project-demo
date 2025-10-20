e1345d21c7145ff43ce2b87aa6598249487f7f1e
ocjdagreat1 <oparachibuikejustine@gmail.com>
2025-10-20 13:36:12 -0800

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,14 @@
-import AppBar from './component/AppBar';
+
+import  Home  from './pages/Home'
 import './App.css'
-import TourCard from "./component/TourCard"
-import Container from '@mui/material/Container';
-import  Grid  from'@mui/material/Grid';
-import cities from "./data.json"
-import { Typography } from '@mui/material';
+
 
 function App() {
   
 
   return (
     <>
-    
-    <AppBar/>
-    <Container sx={{marginY:5}}>
-      {cities.map((city) =>(
-        <>
-        <Typography
-        variant='h4'
-        component="h2"
-        marginTop={5}
-        marginBottom={3}>
-          Top {city.name} Tours
-</Typography>
-<Grid container spacing={1}>
-{city.tours.map((tour,index) =>
-<TourCard tour ={tour} key={index}/> )}
-
-
-</Grid>
-</>
-      ))}
-      
-
-{/*
-
-  */}
-  </Container>
+    <Home/>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import TourCard from "./component/TourCard"
+import Container from '@mui/material/Container';
+import  Grid  from'@mui/material/Grid';
+import cities from "./data.json"
+import { Typography } from '@mui/material';
+import AppBar from './component/AppBar';
+
+const Home = () => {
+  return (
+    
+    <>
+    
+    <AppBar/>
+    <Container sx={{marginY:5}}>
+      {cities.map((city) =>(
+        <>
+        <Typography
+        variant='h4'
+        component="h2"
+        marginTop={5}
+        marginBottom={3}>
+          Top {city.name} Tours
+</Typography>
+<Grid container spacing={1}>
+{city.tours.map((tour,index) =>
+<TourCard tour ={tour} key={index}/> )}
+
+
+</Grid>
+</>
+      ))}
+      
+
+
+  </Container>
+    </>
+
+  )
+}
+
+export default Home
